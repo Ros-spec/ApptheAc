fd92726df41d078ed98c99f61efd4629aec8659b
Ros-spec <rjge022604@gmail.com>
2024-09-13 16:19:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,6 @@ def alumnos():
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
-    return f"Matrícula {request.form["txtMatriculaFA"]} Nombre y Apellido {request.form["txtNombreApellidoFA"]}"
+    matricula = request.form["txtMatriculaFA"]
+    NombreApellido = request.form["txtNombreApellidoFA"]
+    return f"Matrícula {matricula} Nombre y Apellido {NombreApellido}"
