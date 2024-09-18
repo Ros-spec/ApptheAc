0ab5d4701c79424ce9f90b8e74c575f18fce1c85
Ros-spec <rjge022604@gmail.com>
2024-09-18 17:06:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import pusher
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hola, Mundo!</p>"
-
-@app.route("/app")
-def app():
     return render_template("app.html")
 
 @app.route("/alumnos")
@@ -19,6 +17,18 @@ def alumnos():
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
-    matricula = request.form["txtMatriculaFA"]
-    NombreApellido = request.form["txtNombreApellidoFA"]
-    return f"Matrícula {matricula} Nombre y Apellido {NombreApellido}"
+    matricula      = request.form["txtMatriculaFA"]
+    nombreapellido = request.form["txtNombreApellidoFA"]
+    return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
+
+@app.route("/registrar")
+def registrar():
+    pusher_client = pusher.Pusher(
+        app_id="1714541",
+        key="cda1cc599395d699a2af",
+        secret="9e9c00fc36600060d9e2",
+        cluster="us2",
+        ssl=True
+    )
+
+    pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", {"temperatura": 33, "humedad": 0.15})
