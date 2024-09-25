8d3eabe9b67b4e05130f0845bfbb9958b97aef01
Ros-spec <rjge022604@gmail.com>
2024-09-24 19:34:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -42,13 +42,13 @@ def buscar():
 def registrar():
   args = request.args
   
-    pusher_client = pusher.Pusher(
-      app_id = "1766042",
-      key = "b2cda443b1b3457d666e",
-      secret = "4a6a830012d1f16d0619",
-      cluster = "eu",
-      ssl=True
-    )
+    # pusher_client = pusher.Pusher(
+    #   app_id = "1766042",
+    #   key = "b2cda443b1b3457d666e",
+    #   secret = "4a6a830012d1f16d0619",
+    #   cluster = "eu",
+    #   ssl=True
+    # )
   
     # sql = "INSERT INTO tst0_usuarios (Id_Usuario, Nombre_Usuario, Contrasena) VALUES (%s, %s, %s)"
     # val = (args["id"], args["nom"], args["con"] )
