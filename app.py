34b9c54f0bdc22bda8b02362623a6113845ba363
Ros-spec <rjge022604@gmail.com>
2024-09-27 17:06:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,19 +16,26 @@ con = mysql.connector.connect(
   user="u760464709_tst_sep_usr",
   password="dJ0CIAFF="
 )
-    cursor = con.cursor()
-
 app = Flask(__name__)
 
 @app.route("/")
-def usuarios():
-  return render_template("registro.html")
-  
-@app.route("/contenido")
-def contenido():
+def index():
   con.close()
-  return render_template("contenido.html")
+  return render_template("app.html")
 
+@app.route("/alumnos")
+def alumnos():
+  con.close()
+  return render_template("alumnos.html")
+  
+  @app.route("/alumnos/guardar", methods=["POST"])
+def alumnosGuardar():
+    con.close()
+    matricula      = request.form["txtMatriculaFA"]
+    nombreapellido = request.form["txtNombreApellidoFA"]
+
+    return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
+  
 @app.route("/buscar")
 def buscar():
     if not con.is_connected():
@@ -40,31 +47,37 @@ def buscar():
     
     con.close()
     return registros
+  
+@app.route("/contenido")
+def contenido():
+  con.close()
+  return render_template("contenido.html")
+
 
 @app.route("/registrar", methods=["GET"])
 def registrar():
-  args = request.args
+  # args = request.args
 
 
-    sql = "INSERT INTO tst0_usuarios (Id_Usuario, Nombre_Usuario, Contrasena) VALUES (%s, %s, %s)"
-    val = (9, args["txtname"], args["txtpass1"])  # Cambia "4" a 4 si es un entero.
+  #   sql = "INSERT INTO tst0_usuarios (Id_Usuario, Nombre_Usuario, Contrasena) VALUES (%s, %s, %s)"
+  #   val = (9, args["txtname"], args["txtpass1"])  # Cambia "4" a 4 si es un entero.
 
-     try:
-         cursor.execute(sql, val)
-         con.commit()  # Asegúrate de hacer commit si es necesario
-     except Exception as e:
-         print("Ocurrió un error:", e)
-     finally:
-         con.close()  # Asegúrate de cerrar la conexión
+  #    try:
+  #        cursor.execute(sql, val)
+  #        con.commit()  # Asegúrate de hacer commit si es necesario
+  #    except Exception as e:
+  #        print("Ocurrió un error:", e)
+  #    finally:
+  #        con.close()  # Asegúrate de cerrar la conexión
     
 
-    # pusher_client = pusher.Pusher(
-    #     app_id="1766042",
-    #     key="b4444a8caff165daf46a",
-    #     secret="1442ec24356a6e4ac6ce",
-    #     cluster="eu",
-    #     ssl=True
-    # )
+  #   # pusher_client = pusher.Pusher(
+  #   #     app_id="1766042",
+  #   #     key="b4444a8caff165daf46a",
+  #   #     secret="1442ec24356a6e4ac6ce",
+  #   #     cluster="eu",
+  #   #     ssl=True
+  #   # )
 
-    # pusher_client.trigger("canal", "registrocontenido", args)
+  #   # pusher_client.trigger("canal", "registrocontenido", args)
   return args
