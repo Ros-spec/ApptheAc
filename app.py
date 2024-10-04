e3db69da915cf762d865c52a8bc656e32fb39e45
Ros-spec <rjge022604@gmail.com>
2024-10-04 17:00:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, make_response
 import pusher
 import mysql.connector
 import datetime
