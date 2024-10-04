a20b762f3d3d3fc55aab51eda62969f0b90b4d89
Ros-spec <rjge022604@gmail.com>
2024-10-04 17:47:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,20 +13,25 @@ def get_db_connection():
         user="u760464709_tst_sep_usr",
         password="dJ0CIAFF="
     )
+def Eventopusher():
+pusher_client = pusher.Pusher(
+            app_id="1766042",
+            key="b4444a8caff165daf46a",
+            secret="1442ec24356a6e4ac6ce",
+            cluster="eu",
+            ssl=True
+        )
+        
+        pusher_client.trigger("registro", "nuevo", {
+            "ID": id_usuario,
+            "nombre": nombre_usuario,
+            "contraseña": contra
+        })
 
+    
 @app.route("/contenido")
 def index():
     return render_template("contenido.html")
-
-@app.route("/alumnos")
-def alumnos():
-    return render_template("alumnos.html")
-
-@app.route("/alumnos/guardar", methods=["POST"])
-def guardar():
-    matricula = request.form.get("txtMatriculaFA")
-    nombreapellido = request.form.get("txtNombreApellidoFA")
-    return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
 
 @app.route("/buscar")
 def buscar():
@@ -37,35 +42,7 @@ def buscar():
     cursor.close()
     con.close()
     return registros 
-
-# @app.route("/registrar", methods=["GET"])
-# def registrar():
-#     args = request.args
-
-#     if "temperatura" not in args or "humedad" not in args:
-#         return "Faltan parámetros", 400
-
-#     con = get_db_connection()
-#     cursor = con.cursor()
-
-#     sql = "INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora) VALUES (%s, %s, %s)"
-#     val = (args["temperatura"], args["humedad"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
-#     cursor.execute(sql, val)
-#     con.commit()
-#     cursor.close()
-#     con.close()
-
-#     pusher_client = pusher.Pusher(
-#         app_id="1766042",
-#         key="b4444a8caff165daf46a",
-#         secret="1442ec24356a6e4ac6ce",
-#         cluster="eu",
-#         ssl=True
-#     )
     
-#     pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", args)
-#     return args
-
 @app.route("/")
 def data():
     return render_template("registro.html")
@@ -88,22 +65,9 @@ def guardardatos():
         con.commit()
 
         # Disparar el evento con Pusher para actualizar en tiempo real
-        pusher_client = pusher.Pusher(
-            app_id="1766042",
-            key="b4444a8caff165daf46a",
-            secret="1442ec24356a6e4ac6ce",
-            cluster="eu",
-            ssl=True
-        )
-        
-        pusher_client.trigger("registro", "nuevo", {
-            "ID": id_usuario,
-            "nombre": nombre_usuario,
-            "contraseña": contra
-        })
-
+        Eventopusher()
         # Devolver una respuesta JSON de éxito
-        return jsonify({"success": True, "message": "Encuesta guardada exitosamente!"})
+        return make_response(jsonify({"success": True, "message": "Encuesta guardada exitosamente!"}))
 
     except mysql.connector.Error as err:
         print(f"Error al guardar la encuesta: {err}")
